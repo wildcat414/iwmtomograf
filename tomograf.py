135bb38e365de81a3b57bb272a9aa9fc2e69a6bb
Bartosz <bartoszkt14@hotmail.com>
2018-03-20 23:56:45 +0100

    poprawka

--- tomograf.py
+++ tomograf.py
@@ -7,7 +7,7 @@ np.seterr(over='ignore')
 image = cv2.imread("przyklad1.png")
 
 imageHeight = image.shape[0]
-#imageWidth = image.shape[1]
+#imageWidth = image.shape[1] # obraz kwadratowy
 imageWidth = imageHeight
 imageDepth = image.shape[2]
 
@@ -25,36 +25,43 @@ for i in range(imageWidth):
             tavg = math.floor(((image[j,i,0] + image[j,i,1] + image[j,i,2]) / 3))
             imageCircle[j,i] = tavg
 
-numberOfEmitters = 5 # liczba nieparzysta
+numberOfEmitters = 9 # liczba nieparzysta
 halfNumberOfEmitters = math.floor(numberOfEmitters / 2)
-detectorValues = np.zeros(numberOfEmitters, dtype = np.int32)
-angleOfEmitters = 0 # {0; 90; 180; 270}
-spaceBetweenEmitters = 1
+spaceBetweenEmitters = 5
 numberOfSpaces = numberOfEmitters - 1
 emitterOverallWidth = numberOfSpaces * spaceBetweenEmitters + numberOfEmitters
 halfOfOverall = math.floor(emitterOverallWidth / 2)
 
-if angleOfEmitters == 0 or angleOfEmitters == 180:
-    currentCoordX = imageCenterX - halfOfOverall
-    for i in range(numberOfEmitters):
-        currentCoordX = currentCoordX + spaceBetweenEmitters + 1
-        for j in range(imageHeight):
-            d = math.sqrt(pow((currentCoordX - imageCenterX),2) + pow((j - imageCenterY),2))
-            if d <= imageCircleRadius:
-                detectorValues[i] += imageCircle[j,i]
-        detectorValues[i] = math.floor(detectorValues[i] / imageHeight)
-elif angleOfEmitters == 90 or angleOfEmitters == 270:
-    currentCoordY = imageCenterY - halfOfOverall
-    for i in range(numberOfEmitters):
-        currentCoordY = currentCoordY + spaceBetweenEmitters + 1
-        for j in range(imageWidth):
-            d = math.sqrt(pow((currentCoordX - imageCenterX),2) + pow((j - imageCenterY),2))
-            if d <= imageCircleRadius:
-                detectorValues[i] += imageCircle[u,j]
-        detectorValues[i] = math.floor(detectorValues[i] / imageHeight)
+detectorValues = np.zeros(numberOfEmitters + 1, dtype = np.int16)
+angleOfEmitters = 0
+currentCoordX = imageCenterX - halfOfOverall
+for i in range(numberOfEmitters):
+    currentCoordX = currentCoordX + spaceBetweenEmitters + 1
+    normCount = 0
+    for j in range(imageHeight):
+        d = math.sqrt(pow((currentCoordX - imageCenterX),2) + pow((j - imageCenterY),2))
+        if d <= imageCircleRadius:
+            detectorValues[i] += imageCircle[j,currentCoordX]
+            normCount += 1
+    detectorValues[i] = math.floor(detectorValues[i] / normCount)
+detectorValues[numberOfEmitters] = angleOfEmitters
+detectorValuesSaved = detectorValues
+
+detectorValues = np.zeros(numberOfEmitters + 1, dtype = np.int16)
+angleOfEmitters = 90
+currentCoordY = imageCenterY - halfOfOverall
+for i in range(numberOfEmitters):
+    currentCoordY = currentCoordY + spaceBetweenEmitters + 1
+    normCount = 0
+    for j in range(imageWidth):
+        d = math.sqrt(pow((j - imageCenterX),2) + pow((currentCoordY - imageCenterY),2))
+        if d <= imageCircleRadius:
+            detectorValues[i] += imageCircle[currentCoordY,j]
+            normCount += 1
+    detectorValues[i] = math.floor(detectorValues[i] / normCount)
+detectorValues[numberOfEmitters] = angleOfEmitters
+detectorValuesSaved = np.vstack([detectorValuesSaved, detectorValues])
 
 
-
-print('Image original shape', image.shape)
-print('Image circle shape:', imageCircle.shape)
-print('Detector normalized values:', detectorValues)
+print("Image shape (height, width, color): ", image.shape, "\n")
+print("Detector normalized values (last value is angle):\n", detectorValuesSaved)
